b4e87ffa6a6f832cf45be49dc7cdbf9eb91d8648
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-05-11 00:48:17 +0300

    feat: add icons svg

--- frontend/public/icons/icons.tsx
+++ frontend/public/icons/icons.tsx
@@ -13,6 +13,23 @@ export const mediumLogo = (
   </svg>
 );
 
+export const trendingIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className="w-5 h-5"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941"
+    />
+  </svg>
+)
+
 export const notificationIcon = (
   <svg
     width="24"
