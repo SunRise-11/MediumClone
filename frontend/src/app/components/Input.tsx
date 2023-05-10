7cba2cd7d232493dc9316ff94fcfc4ea0132c45e
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-05-11 00:50:29 +0300

    feat: add helper input component

---
+++ frontend/src/app/components/Input.tsx
@@ -0,0 +1,62 @@
+import { Box } from '@mui/material';
+import { TextField } from '@mui/material';
+import { type } from 'os';
+import { ChangeEvent, ReactNode } from 'react';
+
+type Props = {
+  error?: boolean;
+  name: string;
+  inputType?: string;
+  label: string;
+  errorMessage?: string | null;
+  variant?: 'standard' | 'outlined' | 'filled';
+  description?: string;
+  required?: boolean;
+  onChangeForm?: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: string | null;
+}
+
+export default function Input({
+  error,
+  name,
+  inputType,
+  label,
+  errorMessage,
+  variant,
+  description,
+  required,
+  onChangeForm,
+  value, }: Props) {
+  let type: 'standard' | 'outlined' | 'filled' = 'standard';
+  let desc = description;
+  if (variant) type = variant;
+  if (errorMessage) desc = '';
+  return (
+    <Box
+      sx={{
+        width: 500,
+        maxWidth: '100%',
+      }}
+    >
+      <TextField
+        className="xl:w-full min-w-[300px]"
+        value={value}
+        onChange={onChangeForm}
+        fullWidth
+        type={inputType}
+        required={required}
+        error={error || errorMessage ? true : false}
+        label={label}
+        name={name}
+        variant={type}
+        helperText={desc}
+      />
+      {errorMessage && (
+        <p className="text-red-600 px-2 text-[12px] font-mono">
+          <span>* </span>
+          {errorMessage}
+        </p>
+      )}
+    </Box>
+  );
+}
