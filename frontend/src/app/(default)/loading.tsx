17f75282230a39e49c6f0d0dca25c42153400939
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-05-11 19:31:42 +0300

    feat: spiner eklendi

---
+++ frontend/src/app/(default)/loading.tsx
@@ -0,0 +1,6 @@
+
+export default function Loading() {
+    return <div className="w-full h-full flex justify-center items-center">
+        <div className="w-12 h-12 rounded-full animate-spin border-2 border-solid border-blue-500 border-t-transparent"></div>
+    </div>
+}
