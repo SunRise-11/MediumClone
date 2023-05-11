ad775ee71df089503c2edc17b2a8b24bfe5870c4
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-05-11 19:32:35 +0300

    feat: anasayfa tasarlandi

---
+++ frontend/src/components/HomeFeed.tsx
@@ -0,0 +1,25 @@
+import Hero from "./Hero";
+import PostList from "./PostList";
+import Tags from "./Tags";
+import Trending from "./Trending";
+
+const HomeFeed = (): JSX.Element => {
+    return (
+        <>
+            <Hero />
+            <Trending />
+            <div className="relative flex flex-col lg:flex-row px-9 lg:px-20 lg:gap-x-24 my-16">
+                <div className="w-full lg:w-[65%] lg:max-w-[710px]">
+                    <PostList />
+                </div>
+                <div className="flex flex-col w-full lg:w-[35%] order-first lg:order-last mt-12">
+                    <div className="sticky top-20">
+                        <Tags />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default HomeFeed;
