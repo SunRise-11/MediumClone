84f9ffae8100dc84c66cb05d555669764377ddea
SunRise-11 <cleanpinesunrise03@gmail.com>
2023-05-15 17:23:37 +0300

    avatar  eklendi

---
+++ frontend/src/components/Avatar.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useCallback } from "react";
+import defaultImage from "../../../frontend/public/images/profile.png"
+
+interface AvatarProps {
+    username: string;
+    image?: string
+    isLarge?: boolean;
+    isMedium?: boolean;
+    isSmall?: boolean
+    hasBorder?: boolean;
+
+}
+
+const Avatar: React.FC<AvatarProps> = ({ username, image, isLarge, hasBorder, isMedium, isSmall }) => {
+    const router = useRouter();
+
+    const onClick = useCallback((event: any) => {
+        event.stopPropagation();
+
+        const url = `/users/${username}`;
+
+        router.push(url);
+    }, [router, username]);
+
+
+    const handleImageSize = () => {
+        if (isLarge && !isMedium) {
+            return { w: 90, h: 90 }
+        } else if (isMedium && !isLarge) {
+            return { w: 50, h: 50 }
+        } else if (isSmall) {
+            return { w: 15, h: 15 }
+        }
+        return { w: 40, h: 40 }
+    }
+
+    return (
+        <div
+            className={`
+        ${hasBorder ? 'border-2 border-black' : ''}
+        ${isLarge ? 'h-28' : 'h-12'}
+        ${!isLarge && isMedium ? 'h-20' : 'h-12'}
+        ${isLarge ? 'w-28' : 'w-12'}
+        ${isSmall ? "w-20" : "h-5"}
+        hover:opacity-90 
+        transition 
+        cursor-pointer
+        flex justify-center items-center
+      `}
+        >
+            <Image
+                className="rounded-full"
+                width={handleImageSize().w}
+                height={handleImageSize().h}
+                alt="Avatar"
+                onClick={onClick}
+                src={image || defaultImage.src}
+            />
+        </div>
+    );
+}
+
+export default Avatar;
