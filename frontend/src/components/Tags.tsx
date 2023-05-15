5de6abc8219070516bc3d9e8b0089d0036cc02a5
Abbas-Abdelila <abbasabdelila@gmail.com>
2023-05-15 20:30:09 +0300

    fix: change url definition

--- frontend/src/components/Tags.tsx
+++ frontend/src/components/Tags.tsx
@@ -2,37 +2,19 @@
 import React, { useEffect, useState } from 'react'
 import { tags as topics } from '../store';
 import Link from 'next/link';
-import TagFooter from './TagFooter';
 import Tag from '@/types/Tag/Tag';
-import { title } from 'process';
 import { Chip } from '@mui/material';
 
-<<<<<<< HEAD
-type Tag = {
-    id: number,
-    name: string, 
-    url: string
-=======
 type Props = {
     title: string,
     footer?: React.ReactElement;
->>>>>>> bc34665c8b4cc9a46526cab3b9b23cc7dbe85bd0
 }
 
 export default function Tags({ title, footer }: Props) {
 
-    const [tags, setTags] = useState<Tag[]>([]);
+    
 
-    const fetchAllTag = () => {
-        //burası gereksiz :) sadece simule amaçlı 
-        setTags(topics);
-        //ToDo: some code here 
-    };
-
-    useEffect(() => {
-        fetchAllTag()
-    }, [])
-
+   
 
     return (
         <>
@@ -41,21 +23,11 @@ export default function Tags({ title, footer }: Props) {
             </h1>
             <div className="flex flex-wrap gap-2 border-slate-300 pb-10">
                 {
-                    tags.map((tag: Tag) => {
-<<<<<<< HEAD
+                    topics.map((tag: Tag) => {
                         const { name, id, url } = tag;
-
                         return (
                             <Link key={id} href={url}>
-                                <p className="px-4 py-[6px] border border-slate-300 rounded-xl text-slate-500 text-sm cursor-pointer">
-                                    {name}
-                                </p>
-=======
-                        const { name, id } = tag;
-                        return (
-                            <Link key={id} href={`tags/${name}`}>
-                                <Chip key={tag.id} label={tag.name} clickable />
->>>>>>> bc34665c8b4cc9a46526cab3b9b23cc7dbe85bd0
+                                <Chip key={tag.id} label={tag.name} clickable size='medium' />
                             </Link>
                         );
                     })}
